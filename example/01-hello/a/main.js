b8afa28a23021a6599bd505ca7eca07c8585c5ea
agron911 <harrison0911@gmail.com>
2017-09-27 14:55:28 +0800

    'asd'

--- example/01-hello/a/main.js
+++ example/01-hello/a/main.js
@@ -3,6 +3,7 @@ const {app, BrowserWindow} = require('electron')
 function createWindow () {
   var win = new BrowserWindow({width: 800, height: 600})
   win.loadURL('file://' + __dirname + '/index.html')
+  win.webContents.openDevTools()
 }
 
 app.on('ready', createWindow)
