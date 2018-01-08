58388a204446cffb63bfafcd6a608469f1227d40
agron911 <harrison0911@gmail.com>
2018-01-08 20:59:26 +0800

    'project'

---
+++ project/test1/main.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow () {
+  var win = new BrowserWindow({width: 800, height: 600})
+  win.loadURL('file://' + __dirname + '/index.html')
+
+}
+
+
+
+app.on('ready', createWindow)
+
+
+
+
